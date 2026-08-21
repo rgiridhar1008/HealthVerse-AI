6247d0c76904864de04a3a37142e6abacf5d8b02
RACHAKONDA LAKSHMI SAI GIRIDHAR <2420030171@klh.edu.in>
2026-08-22 00:53:33 +0530

    Delete HealthVerse-AI/push.ps1

--- HealthVerse-AI/push.ps1
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Update UI and Landing Page"
-git branch -M main
-git remote remove origin 2>$null
-git remote add origin https://github.com/rgiridhar1008/HealthVerse-AI.git
-git push -u origin main
